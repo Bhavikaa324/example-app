1c728e2c50d62e80063f84cbe4b24475bd0ffb76
“Bhavikaa324” <puppalwarbhavika@gmail.com>
2023-11-01 21:01:47 +0530

    Make inverted triangle code readable and maintainable

--- practicals/invertedtriangle.php
+++ practicals/invertedtriangle.php
@@ -1,6 +1,6 @@
 <?php
-$rows = 5; // Change the number of rows as per your requirement
-
+$rows = 5; // Number of rows in the pattern
+// Generating the pattern
 for ($i = $rows; $i >= 1; $i--) {
     for ($j = 1; $j <= $i; $j++) {
         echo "*";
